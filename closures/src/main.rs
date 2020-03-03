23f04c9ccd56fd299188cf6c82f2e22ad44f1194
Dave Filion <dave.filion@gmail.com>
2020-03-03 11:46:38 -0500

    closure file

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,3 +1,35 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+}
+
+fn generate_workout(intensity: u32, randome_number: u32) {
+    let expensive_closure = |num| {
+        println!("slow calc");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    let expensive_result = simulated_expensive_calculation(intensity);
+
+    if intensity < 25 {
+        println!("Today, do {} pushups", expensive_result);
+        println!("Next, do {} situps", expensive_result);
+    } else {
+        print!("take a break todaay");
+    }
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
